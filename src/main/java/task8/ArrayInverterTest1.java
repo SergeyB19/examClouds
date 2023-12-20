4c88ebb74ca2144a20cb1def60b736325bb8819f
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-12-20 11:19:44 +0300

    examClouds

---
+++ src/main/java/task8/ArrayInverterTest1.java
@@ -0,0 +1,38 @@
+package task8;
+
+import java.util.Arrays;
+
+public class ArrayInverterTest1 {
+    public static void main(String[] args) {
+        int[] array1 = new int[]{};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+        array1 = new int[]{0};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+        array1 = new int[]{0,1};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+        array1 = new int[]{0,1,2};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+        array1 = new int[]{0,1,2,3};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+        array1 = new int[]{0,1,2,3,4,5,6,7,8,9};
+        System.out.println(Arrays.toString(array1));
+        ArrayInverter.invert(array1);
+        System.out.println(Arrays.toString(array1));
+
+    }
+}
