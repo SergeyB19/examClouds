d736bd695308a876a8178a855f0250b872abd728
SergeyB19 <my.h.m@yandex.ru>
2024-02-11 20:39:27 +0300

    Межпотоковые коммуникации

---
+++ src/main/java/task53/Consumer.java
@@ -0,0 +1,16 @@
+package task53;
+
+public class Consumer implements Runnable {
+    private MyQueue myQueue;
+
+    public Consumer(MyQueue myQueue) {
+        this.myQueue = myQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            myQueue.get();
+        }
+    }
+}
---
+++ src/main/java/task53/Producer.java
@@ -0,0 +1,16 @@
+package task53;
+
+public class Producer implements Runnable {
+    private MyQueue myQueue;
+
+    public Producer(MyQueue myQueue) {
+        this.myQueue = myQueue;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            myQueue.put(i);
+        }
+    }
+}
